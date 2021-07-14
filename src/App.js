4e069992dac56fde7267fec6549857dfe0ddadb7
unknown <Yohana Fransisca@PLZNB0489200200.Supporting.corp.bankmandiri.co.id>
2021-07-14 12:48:00 +0700

    add modul 2 homework 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Track from './components/Track';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Playlist</h1>
+      <div className="container">
+        <Track />
+      </div>
     </div>
   );
 }
---
+++ src/components/Track.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Track.css'
+import Data from './Data'
+
+function Track() {
+    
+    return (
+        <div className="track">
+            <img className="track-image" alt="track" src={Data.album.images[1].url} />
+            <p className="track-title">{Data.name}</p>
+            <p className="track-artist">{Data.artists[0].name}</p>
+            <p className="track-album">{Data.album.name}</p>
+            <button className="btn">Select</button>
+        </div>
+    )
+}
+
+export default Track
