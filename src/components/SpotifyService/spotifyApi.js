cb52127752211cfda2c217640c08bb41a57f5ee8
Yohana Fransisca Sarumpaet <yohanafransiscasarumpaet@gmail.com>
2021-08-15 13:55:27 +0700

    fix redirect url

--- src/components/SpotifyService/spotifyApi.js
+++ src/components/SpotifyService/spotifyApi.js
@@ -3,7 +3,7 @@ const API_ENDPOINT = 'https://api.spotify.com/v1';
 const RESPONSE_TYPE = 'token';
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const SCOPE = 'playlist-modify-private';
-const REDIRECT_URI = 'http://localhost:3000/';
+const REDIRECT_URI = 'https://generasi-gigih-homeworks-byviriszj-yonafransisca.vercel.app/';
 
 export {
     AUTH_ENDPOINT,
